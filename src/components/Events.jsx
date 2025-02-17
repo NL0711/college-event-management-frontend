754b4137bdc8892f21f09f43a9dca3b03ea60d42
NL0711 <nathashalewis777@gmail.com>
2025-02-17 20:06:52 +0530

    event cards created from NeonDB

--- src/components/Events.jsx
+++ src/components/Events.jsx
@@ -1,55 +1,29 @@
 import EventsCards from "./EventsCards";
+import { useState, useEffect } from "react";
 
 const Events = () => {
+
+  const [events, setEvents] = useState([]);
+  useEffect(() => {
+    fetch('http://localhost:5000/events')
+     .then(response => response.json())
+     .then(data => setEvents(data))
+     .catch(error => console.error('Error:', error));
+  }, [])
+
   return (
     <div className="grid grid-cols-3 px-6 gap-x-4 gap-y-6">
-      <EventsCards 
-        title="Fr. Conceicao Rodrigues Memorial Debate" 
-        date="September 2024" 
-        location="Samvaad Hall, Campus" 
-        time="10:00 AM - 4:00 PM" 
-        src="/images/event_pic/debate.png" 
-      />
-
-      <EventsCards 
-        title="Euphoria Cultural Fest" 
-        date="15th - 17th November 2024" 
-        location="Campus" 
-        time="9:00 AM - 10:00 PM" 
-        src="/images/event_pic/cultural.png"
-      />
-
-      <EventsCards 
-        title="Crescendo Technical Festival" 
-        date="5th - 7th January 2025" 
-        location="Campus" 
-        time="9:30 AM - 6:00 PM" 
-        src="/images/event_pic/technical.png"
-      />
-
-      <EventsCards 
-        title="Athlos Sports Fest" 
-        date="20th - 22nd February 2025" 
-        location="Sports Grounds" 
-        time="7:00 AM - 6:00 PM" 
-        src="/images/event_pic/sports.png"
-      />
-
-      <EventsCards 
-        title="The Heart & Sole Run 5" 
-        date="19th March 2024" 
-        location="Fr. Agnel Ashram" 
-        time="6:00 AM - 10:00 AM" 
-        src="/images/event_pic/marathon.png"
-      />
-
-      <EventsCards 
-        title="Beach Clean Up" 
-        date="9th October 2022" 
-        location="Mumbai Beaches" 
-        time="7:00 AM - 11:00 AM" 
-        src="/images/event_pic/beach.png" 
-      />
+      {events.map(event => (
+        <EventsCards 
+          key={event.id}
+          title={event.title} 
+          start_date={event.start_date}
+          end_date={event.end_date}
+          location={event.location}
+          time_start={event.time_start} 
+          time_end={event.time_end}
+          src={event.image_src}
+          /> ))}
     </div>
   )
 }
--- src/components/EventsCards.jsx
+++ src/components/EventsCards.jsx
@@ -1,21 +1,27 @@
 import { Card, CardContent, CardHeader, CardTitle } from "./ui/card";
+import moment from "moment";
 
-const EventCards = ({title, date, location, time, src}) => {
+const EventCards = ({title, start_date, end_date, location, time_start, time_end, src}) => {
+
+  function formatTime(time) {
+    return moment(time, "HH:mm:ss").format("h:mm A");
+  }
+
   return (
     <div>
-      <Card className="relative flex flex-col items-center min-h-[22rem] max-h-[24rem] shadow-md shadow-zinc-200 border-0 rounded-t-2xl
-      hover: border-lime-600">
+      <Card className="relative flex flex-col items-center min-h-[22rem] max-h-[24rem]
+       shadow-md shadow-zinc-200 border-0 rounded-t-2xl">
         <CardHeader className="p-0 border-0">
           <img className="aspect-video rounded-t-2xl cursor-pointer" src={src} />
           <CardTitle className="text-center font-bold text-indigo-950 text-xl">{title}</CardTitle>
         </CardHeader>
         <CardContent className="flex flex-col items-center text-center pt-4 gap-2">
-          <p>{date}</p>
+          <p>{start_date}{end_date !== start_date && ` - ${end_date}`}</p>
           <div className="flex gap-2">
             <img src="/icons/location.png" className="w-4 h-4 translate-y-1" />
             <p>{location}</p>
           </div>
-          <p>{time}</p>
+          <p>{formatTime(time_start)}{time_end && ` - ${formatTime(time_end)}`}</p>
           <a href="#" className="absolute text-indigo-600 text-sm right-2 bottom-2 p-2">Register</a>
         </CardContent>
       </Card>
